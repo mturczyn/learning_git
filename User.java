9c6c9b611896fbcd80a391270177ce18ca5d895d
MichalTurczyn <turek1992@o2.pl>
2020-07-26 23:43:51 +0200

    MASTER another User capability

--- User.java
+++ User.java
@@ -24,6 +24,10 @@ namespace UserManagement {
 			throw new Exception("Users don't think");
 		}
 		
+		public String thinkOutOfTheBox() {
+			throw new Exception("Users don't think");
+		}
+		
 		@Override
 		public String toString() {
 			return "My name is " + firstName + " and my age is " + age;
