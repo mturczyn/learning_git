c003edccd96dd2505aac48433582452280d173ad
MichalTurczyn <turek1992@o2.pl>
2020-07-26 20:44:01 +0200

    removed field from user

--- User.java
+++ User.java
@@ -2,7 +2,6 @@ namespace UserManagement {
 	public class User {
 		private String firstName;
 		private Integer age;
-		private String lastName;
 		
 		public User(String firstName, Integer age) {
 			this.age = age;
