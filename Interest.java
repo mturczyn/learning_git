a50deb549ce800383e186cfd1c491f9e3572b006
Michal Turczyn <turek1992@o2.pl>
2020-08-09 11:17:45 +0200

    Dodanie pola user do klasy Interest

--- Interest.java
+++ Interest.java
@@ -2,15 +2,19 @@ namespace UserManagement {
 	public class Interest {
 		private String name;
 		private String description;
+		private User user;
 		
-		public Interest(String name, String description) {
+		public Interest(String name, String description, User user) {
 			this.description = description;
 			this.name = name;
+			this.user = user;
 		}
 		
 		public String getName() {
 			return name;
 		}
+		
+		
 		
 		@Override
 		public String toString() {
