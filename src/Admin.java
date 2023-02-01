45a94112f1b473c786c79dd9adafff1f39480d76
Michal Turczyn <turek1992@o2.pl>
2023-02-01 14:08:08 +0100

    Added comments

--- src/Admin.java
+++ src/Admin.java
@@ -1,23 +1,5 @@
 namespace UserManagement {
-<<<<<<< HEAD
 	// This meant to cause conflicts asdf asdfvscefgsegcdrgcs
-	// This meant to cause conflicts asdf asdfvscefgsegcdrgcs
-	// This meant to cause conflicts asdf asdfvscefgsegcdrgcs
-	// This meant to cause conflicts asdf asdfvscefgsegcdrgcs
-	// This meant to cause conflicts asdf asdfvscefgsegcdrgcs
-	// This meant to cause conflicts asdf asdfvscefgsegcdrgcs
-	// This meant to cause conflicts asdf asdfvscefgsegcdrgcs
-	// This meant to cause conflicts asdf asdfvscefgsegcdrgcs
-=======
-	// This meant to cause conflicts
-	// This meant to cause conflicts
-	// This meant to cause conflicts
-	// This meant to cause conflicts
-	// This meant to cause conflicts
-	// This meant to cause conflicts
-	// This meant to cause conflicts
-	// This meant to cause conflicts
->>>>>>> 55a64b215160c43f33a370abbecc9808334dc67e
 	public class Admin : User {
 		public void doAdminStuf() {
 			// do admin stuff...
