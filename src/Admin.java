d7362609e267677b6a13802ced69dba5f2d555f4
Michal Turczyn <turek1992@o2.pl>
2024-06-09 11:46:07 +0200

    Test commit - edited Admin class.

--- src/Admin.java
+++ src/Admin.java
@@ -1,13 +1,9 @@
 namespace UserManagement {
-	// comment
 	public class Admin : User {
-		// comment
 		public void doAdminStuf() {
 		}
-		// comment
 		public void deleteEveything() {
 		}
-		// comment
 		public void createEverything() {
 		}
 	}
